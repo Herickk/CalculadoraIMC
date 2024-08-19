6f3c7151c61accbf4640f9a7ebbc272875262f55
Herick <herickwnascimento@gmail.com>
2024-08-19 22:55:11 +0100

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,14 +17,34 @@ class MainActivity : AppCompatActivity() {
 
         val edt_peso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edt_altura = findViewById<TextInputEditText>(R.id.edt_altura)
-        val btn_calcular =findViewById<Button>(R.id.btn_calcular)
+        val btn_calcular = findViewById<Button>(R.id.btn_calcular)
 
-        btn_calcular.setOnClickListener{
-            val peso = edt_peso.text
-            println("Herick açao do Botao" + peso)
-           val altura = edt_peso.text
-            println("Essa e a altura indicada" + altura )
+        btn_calcular.setOnClickListener {
+            val pesoStr: String = edt_peso.text.toString()
+            val alturaStr: String = edt_altura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                //Mostrar mensagem para usuario
+                Snackbar
+                    .make(
+                    edt_peso,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            } else {
+                val peso= pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+
+
+
+                println("Essa e a altura indicada" + alturaStr)
+            }
+
         }
-
     }
 }
